d5aab468d3f53da9a4e0aa303ffbea6a5e1f5d13
zadjii <zadjii@gmail.com>
2018-08-30 17:32:12 -0700

    mdx supports rudimentary versioning

--- src/mdx.js
+++ src/mdx.js
@@ -4,18 +4,26 @@ function Mdx(mdxjson){
     this._initialText = null;
     this._text = null;
     this._comments = null;
+    this._lastComments = null;
     this._updated = false;
+    this._metadata = null;
 
     if (mdxjson){
         if (mdxjson.text){
             this._text = mdxjson.text;
         }
         if (mdxjson.lastText){
-
+            this._initialText = mdxjson.lastText;
+        }
+        else {
+            this._initialText = this._text;
         }
         // TODO: validate that each comment has a start and end point, and
         //      those points have a x and y
         if (mdxjson.comments) this._comments = mdxjson.comments;
+        if (mdxjson.metadata) this._metadata = mdxjson.metadata;
+
+        this._lastComments = this._comments;
     }
 
     this.comments = function () {
@@ -23,6 +31,12 @@ function Mdx(mdxjson){
     }
     this.text = function () {
         return this._text;
+    }
+    this.lastText = function () {
+        return this._initialText;
+    }
+    this.lastComments = function(){
+        return this._lastComments;
     }
 
     this.addComment = function (start, end, text, metadata) {
@@ -32,15 +46,42 @@ function Mdx(mdxjson){
     }
 
     this.updateText = function (newText) {
+        if (!this._updated){
+            this._initialText = this._text;
+            this._updated = true;
+        }
+        // Replace our set of lastComments with the current comment locations
+        this._lastComments = this._comments;
+        // set our text to be the new text's value
+        let lastText = this._text;
+        this._text = newText;
 
+        // calculate the new locations of each of the comments
+        this._comments = [];
+        let diffs = new DiffSet(lastText, newText);
+        for (var i = 0; i < this._lastComments.length; i++) {
+            let comment = this._lastComments[i];
+            let newStart = diffs.translate_point(comment.start);
+            let newEnd = diffs.translate_point(comment.end);
+            // TODO handle points that are lost now (-1,-1)
+            // TODO handle comments where both are abandoned
+            this.addComment(newStart, newEnd, comment.text, comment.metadata);
+        }
     }
 
-    this.export = function () {
-        return {text:this._text, comments:this._comments};
+    this.export = function (exportLast) {
+        let result = {text:this._text, comments:this._comments}
+        if (exportLast){
+            result.lastText = this._initialText;
+        }
+        return result;
     }
 }
 
-Mdx.import = function(text, comments){
+Mdx.import = function(text, comments, lastText){
     let mdxjson = {text:text, comments:comments};
+    if (lastText){
+        mdxjson.lastText = lastText;
+    }
     return new Mdx(mdxjson);
 }
